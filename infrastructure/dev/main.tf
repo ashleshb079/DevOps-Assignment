82d0bcaea0668d0c3425bd476d470af8fc54fbdc
ashlesh <ashleshb079@gmail.com>
2026-05-04 16:43:09 +0530

    fix s3 bucket blocking public access

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -262,18 +262,11 @@ resource "aws_s3_bucket_website_configuration" "frontend_site" {
 #######################
 # Public bucket policy
 #######################
-resource "aws_s3_bucket_policy" "frontend_policy" {
+resource "aws_s3_bucket_public_access_block" "frontend" {
   bucket = aws_s3_bucket.frontend.id
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = "*"
-        Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.frontend.arn}/*"
-      }
-    ]
-  })
+  block_public_acls       = true
+  block_public_policy      = true
+  ignore_public_acls       = true
+  restrict_public_buckets  = true
 }
